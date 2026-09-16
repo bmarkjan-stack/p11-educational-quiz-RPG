38c655e2f272ddb90292c5a13dd67c13fc92db8d
bmarkjan-stack <282548594+bmarkjan-stack@users.noreply.github.com>
2026-09-16 15:53:44 +0800

    feat: create boss entity

---
+++ src/entities/Boss.js
@@ -0,0 +1,72 @@
+export default class Boss {
+    constructor(
+        scene,
+        x,
+        y,
+        { textureKey = "boss1-slime", name = "Slime", maxHp = 100, attackPower = 10 } = {}
+    ) {
+        this.scene = scene;
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.attackPower = attackPower;
+
+        this.sprite = scene.add.sprite(x, y, textureKey).setOrigin(0.5).setScale(0.6);
+
+        this.baseX = x;
+        this.baseY = y;
+    }
+
+    attack(target) {
+        this.playAttackAnimation(() => {
+            target.takeDamage(this.attackPower);
+        });
+    }
+
+    takeDamage(amount) {
+        this.hp = Math.max(0, this.hp - amount);
+        this.playHurtAnimation();
+        return this.hp;
+    }
+
+    isAlive() {
+        return this.hp > 0;
+    }
+
+    playAttackAnimation(onComplete) {
+        this.scene.tweens.add({
+            targets: this.sprite,
+            x: this.baseX - 40,
+            duration: 150,
+            yoyo: true,
+            onYoyo: onComplete,
+        });
+    }
+
+    playHurtAnimation() {
+        this.sprite.setTintFill(0xff6b6b);
+
+        this.scene.tweens.add({
+            targets: this.sprite,
+            x: this.baseX + 10,
+            duration: 60,
+            yoyo: true,
+            repeat: 2,
+            onComplete: () => this.sprite.clearTint(),
+        });
+    }
+
+    playDefeatAnimation(onComplete) {
+        this.scene.tweens.add({
+            targets: this.sprite,
+            alpha: 0,
+            scale: 0.2,
+            duration: 500,
+            onComplete,
+        });
+    }
+
+    destroy() {
+        this.sprite.destroy();
+    }
+}
